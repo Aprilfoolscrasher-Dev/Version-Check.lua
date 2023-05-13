b67effc2ae1ccc7d773b474e7c93f47d76defd72
Aprilfoolscrasher-Dev <132524982+Aprilfoolscrasher-Dev@users.noreply.github.com>
2023-05-13 02:36:46 -0400

    Create Version-Check.lua

---
+++ Version-Check.lua
@@ -0,0 +1,5 @@
+version = "Jared 3.4 (Beta,legacy)"--DON"T TAMPER WITH, VERSION CHECK!
+
+if version ~= "Jared 3.4 (Beta,legacy)" then --DON"T TAMPER WITH, VERSION CHECK!
+    game.Players.LocalPlayer:Kick("New update. Please check the discord Server")
+end
